c903e0821f026797f3012a63298993bc53b80afb
Luis Miranda <luistm@gmail.com>
2014-06-09 22:10:52 +0100

    Adds author and author email to project metadata.

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from distutils.core import setup
 setup(
     name='GitPyWrapper',
     version='0.1dev',
+    author='Luis Miranda',
+    author_email='luistm@gmail.com',
     packages=['gitpywrapper', ],
     license='MIT license',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
 )
