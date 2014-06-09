cc32de100599a6a85dcb4171a10b88ead5d5f0fc
Luis Miranda <luistm@gmail.com>
2014-06-09 22:07:01 +0100

    Adds setup file with project metadata.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='GitPyWrapper',
+    version='0.1dev',
+    packages=['gitpywrapper', ],
+    license='MIT license',
+    long_description=open('README.txt').read(),
+)
